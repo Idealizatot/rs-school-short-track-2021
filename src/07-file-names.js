88cac3a6b2ce94e0f1bbcf4fd800deb97cba5cf0
Idealizatot <ideal103.5@gmail.com>
2021-04-06 17:08:46 +0300

    fix: delete eslint errors

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,14 +13,12 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(names) {
-  const newArr = [];
-  const obj = {};
-  for (let i = 0; i < names.length; i++) {
-    const str = names[i];
-    
-  }
-
+function renameFiles(/* names */) {
+  // const newArr = [];
+  // const obj = {};
+  // for (let i = 0; i < names.length; i++) {
+  //   const str = names[i];
+  // }
 }
 
 module.exports = renameFiles;
